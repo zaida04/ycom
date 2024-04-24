46426180e34e098e8337bea24ac398cc6ad3795c
Zaid Arshad (Nico) <nico.03727+github@gmail.com>
2024-04-23 21:22:46 -0400

    working on local dev

--- src/env.ts
+++ src/env.ts
@@ -3,5 +3,5 @@ export const IsDev = !IsProd;
 
 export const dbUrl = process.env.DB_URL!;
 export const wsApiUrl = process.env.WS_API_URL!;
-export const wsPublicUrl = process.env.WS_PUBLIC_URL!;
-export const apiUrl = process.env.API_URL!;
+export const wsPublicUrl = process.env.NEXT_PUBLIC_WS_PUBLIC_URL!;
+export const apiUrl = process.env.NEXT_PUBLIC_API_URL!;
