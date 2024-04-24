a90fb511092c520c2b6d68427e2b12e3123e3f76
Zaid Arshad <nico.03727+github@gmail.com>
2024-04-24 13:48:35 -0400

    feat: use ratelimiting

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -2,8 +2,20 @@ import Session from '@/db/Session';
 import User, { IUser } from '@/db/User';
 import { initTRPC } from '@trpc/server';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { createTRPCStoreLimiter, defaultFingerPrint } from '@trpc-limiter/memory'
 
 const t = initTRPC.context<{ user: IUser | null; req: NextApiRequest; res: NextApiResponse }>().create();
+
+t.procedure.use(createTRPCStoreLimiter<typeof t>({
+    fingerprint: (ctx) => defaultFingerPrint(ctx.req),
+    message: (hitInfo) =>
+        `Too many requests, please try again later. ${Math.ceil(
+            (hitInfo - Date.now()) / 1000
+        )}`,
+    max: 40,
+    windowMs: 10000,
+})
+)
 
 export const protectedProcedure = t.procedure.use(async function isAuthed(opts) {
     const { req, res } = opts.ctx;
